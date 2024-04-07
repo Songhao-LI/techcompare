bc9a786383e22abe6142e0b1399016d6c04e02c8
Songhao-LI <sl10500@nyu.edu>
2024-04-07 16:33:02 -0400

    [update]:router

--- Web/Tech/src/components/Navbar/Navbar.jsx
+++ Web/Tech/src/components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const MenuLinks = [
   {
     id: 4,
     name: "Blogs",
-    link: "/#blog",
+    link: "/blog",
   },
 ];
 
---
+++ Web/Tech/src/components/ProductsDetail/ProductsDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductsDetail = () => {
+    return (
+        <div>
+            {}
+        </div>
+    );
+};
+
+export default ProductsDetail;
