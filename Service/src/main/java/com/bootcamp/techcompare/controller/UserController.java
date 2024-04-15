a390e141d870870363ac71d3fc3cbd2a0a4a864f
Linyi He <hexiaohexiao@me.com>
2024-04-15 17:28:48 -0400

    add Cognito integration, replacing self hosted user management

--- Service/src/main/java/com/bootcamp/techcompare/controller/UserController.java
+++ Service/src/main/java/com/bootcamp/techcompare/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.bootcamp.techcompare.controller;
 
+import com.bootcamp.techcompare.model.User;
+import com.bootcamp.techcompare.model.UserLoginRequest;
 import com.bootcamp.techcompare.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -8,11 +10,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -21,37 +20,68 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Operation(
-            summary = "User login",
-            description = "Login user with username and password.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema()) }),
-            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) })})
-    @PostMapping("/login")
-    public String login(
-            @RequestParam(value = "username", required = true) String username,
-            @RequestParam(value = "password", required = true) String password,
-            HttpSession session) {
-        boolean loginSuccess = userService.login(username, password);
-        if (loginSuccess) {
-//            set user id in session cookie with base64 username
-//            String encodedUsername = java.util.Base64.getEncoder().encodeToString(username.getBytes());
-            session.setAttribute("username", username);
-            return "redirect:/search.html";
-        } else {
-            return "redirect:/";
+//    hello world
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, world!";
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User userRequest) {
+        try {
+            userService.registerUser(userRequest); // Register user with Cognito
+            return ResponseEntity.ok("User registered successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error registering user: " + e.getMessage());
         }
     }
 
-    @Operation(
-            summary = "User logout",
-            description = "Logout user. Always returns success.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema()) })})
-    @PostMapping("/logout")
-    public String logout(HttpSession session) {
-        userService.logout();
-        session.invalidate();
-        return "redirect:/";
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody UserLoginRequest userRequest) {
+        if (userService.loginUser(userRequest)) { // Login user with Cognito
+            return ResponseEntity.ok("User logged in successfully.");
+        } else {
+            return ResponseEntity.badRequest().body("Invalid username or password.");
+        }
     }
+
+//    @Operation(
+//            summary = "User login",
+//            description = "Login user with username and password.")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema()) }),
+//            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) })})
+//    @PostMapping("/login")
+//    public String login(
+//            @RequestParam(value = "username", required = true) String username,
+//            @RequestParam(value = "password", required = true) String password,
+//            HttpSession session) {
+//        boolean loginSuccess = userService.login(username, password);
+//        if (loginSuccess) {
+////            set user id in session cookie with base64 username
+////            String encodedUsername = java.util.Base64.getEncoder().encodeToString(username.getBytes());
+//            session.setAttribute("username", username);
+//            return "redirect:/search.html";
+//        } else {
+//            return "redirect:/";
+//        }
+//    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logoutUser() {
+        userService.logout(); // Logout user
+        return ResponseEntity.ok("User logged out successfully.");
+    }
+
+//    @Operation(
+//            summary = "User logout",
+//            description = "Logout user. Always returns success.")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema()) })})
+//    @PostMapping("/logout")
+//    public String logout(HttpSession session) {
+//        userService.logout();
+//        session.invalidate();
+//        return "redirect:/";
+//    }
 }
--- Service/src/main/java/com/bootcamp/techcompare/service/UserService.java
+++ Service/src/main/java/com/bootcamp/techcompare/service/UserService.java
@@ -2,44 +2,149 @@ package com.bootcamp.techcompare.service;
 
 import com.bootcamp.techcompare.dao.UserDao;
 import com.bootcamp.techcompare.model.User;
+import com.bootcamp.techcompare.model.UserLoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class UserService {
 
-    private final RestTemplate restTemplate;
+//    private final RestTemplate restTemplate;
+
+//    @Autowired
+//    private CognitoIdentityProviderClient cognitoIdentityProviderClient;
 
     @Autowired
     private UserDao userDao;
+
+    public static String calculateSecretHash(String userPoolClientId, String userPoolClientSecret, String userName) {
+        final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
+
+        SecretKeySpec signingKey = new SecretKeySpec(
+                userPoolClientSecret.getBytes(StandardCharsets.UTF_8),
+                HMAC_SHA256_ALGORITHM);
+        try {
+            Mac mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
+            mac.init(signingKey);
+            mac.update(userName.getBytes(StandardCharsets.UTF_8));
+            byte[] rawHmac = mac.doFinal(userPoolClientId.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(rawHmac);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while calculating ");
+        }
+    }
 
     @Transactional
     public void add(User user) {
         userDao.persist(user);
     }
 
-    @Autowired
-    public UserService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+//    @Autowired
+//    public UserService(RestTemplate restTemplate) {
+//        this.restTemplate = restTemplate;
+//    }
 
-    @Transactional
-    public boolean login(String username, String password) {
-//        return username.equals("admin") && password.equals("admin");
-//        TODO: Implement login with database
-        return userDao.exist(new User(username, password, null));
-    }
+//    @Transactional
+//    public boolean login(String username, String password) {
+////        return username.equals("admin") && password.equals("admin");
+////        TODO: Implement login with database
+//        return userDao.exist(new User(username, password, null));
+//    }
 
-    public String getEmailByUsername(String username) {
-        return userDao.getEmailByUsername(username);
-    }
+//    public String getEmailByUsername(String username) {
+//        return userDao.getEmailByUsername(username);
+//    }
 
     // User logout
     public void logout() {
-        // Do nothing
-        // delete user id from session
+//        cognito logout
+//    TODO: Implement logout
+    }
+
+    public void registerUser(User userRequest) {
+        try {
+            CognitoIdentityProviderClient cognitoIdentityProviderClient = CognitoIdentityProviderClient.builder()
+                    .region(Region.US_WEST_1)
+                    .build();
+
+            // Prepare user's attributes
+            List<AttributeType> userAttributes = new ArrayList<>();
+            userAttributes.add(AttributeType.builder().name("email").value(userRequest.getEmail()).build());
+            // Prepare the request
+            AdminCreateUserRequest createUserRequest = AdminCreateUserRequest.builder()
+                    .userPoolId("us-west-1_TtGfsqs3u") // use environment variable
+                    .username(userRequest.getUsername())
+                    .temporaryPassword(userRequest.getPassword())
+                    .userAttributes(userAttributes)
+                    .build();
+
+            // Use Cognito API
+            cognitoIdentityProviderClient.adminCreateUser(createUserRequest);
+
+            // Set the user's password
+            AdminSetUserPasswordRequest adminSetUserPasswordRequest = AdminSetUserPasswordRequest.builder()
+                    .userPoolId("us-west-1_TtGfsqs3u")
+                    .username(userRequest.getUsername())
+                    .password(userRequest.getPassword())
+                    .permanent(true)
+                    .build();
+
+            // Use Cognito API
+            cognitoIdentityProviderClient.adminSetUserPassword(adminSetUserPasswordRequest);
+
+            // Add the user to a group
+            cognitoIdentityProviderClient.adminAddUserToGroup(AdminAddUserToGroupRequest.builder()
+                    .groupName("techcompare_users")
+                    .userPoolId("us-west-1_TtGfsqs3u")
+                    .username(userRequest.getUsername())
+                    .build());
+
+        } catch (Exception e) {
+            // Handle register errors
+            throw new RuntimeException("Error registering user : " + e.getMessage(), e);
+        }
+    }
+
+    public boolean loginUser(UserLoginRequest userRequest) {
+        try {
+            CognitoIdentityProviderClient cognitoIdentityProviderClient = CognitoIdentityProviderClient.builder()
+                    .region(Region.US_WEST_1)
+                    .build();
+
+            AdminInitiateAuthRequest authRequest = AdminInitiateAuthRequest.builder()
+                    .authFlow("ADMIN_USER_PASSWORD_AUTH")
+                    .clientId("6etson0rhh8mf5trgl9h7ssq44")
+                    .userPoolId("us-west-1_TtGfsqs3u")
+                    .authParameters(new HashMap<>() {{
+                        put("USERNAME", userRequest.getUsername());
+                        put("PASSWORD", userRequest.getPassword());
+                        put("SECRET_HASH", calculateSecretHash("6etson0rhh8mf5trgl9h7ssq44", "1ibadrtitk3q7pisl4m9cf0jk4fr23drm4a1a97d70rgoah3j4er", userRequest.getUsername()));
+//                        put("DEVICE_KEY", "techcompare web");
+                    }}
+                    ).build();
+
+            AdminInitiateAuthResponse authResponse = cognitoIdentityProviderClient.adminInitiateAuth(authRequest);
+            if (authResponse.authenticationResult() != null) {
+                return true;
+            }
+
+        } catch (Exception e) {
+            // Handle login errors
+            return false;
+        }
+        return false;
     }
 }
-
