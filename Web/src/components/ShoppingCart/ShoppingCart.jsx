bc6624f1f655d3b4cacfde6e253f91c542f45379
Sohot <sl10500@nyu.edu>
2024-04-29 17:25:24 -0400

    [update]:loading

--- Web/src/components/ShoppingCart/ShoppingCart.jsx
+++ Web/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import AOS from "aos";
 import axios from "axios";
 import {useDispatch, useSelector} from "react-redux";
 import {AiOutlineFileExcel} from "react-icons/ai"
 import {Link} from "react-router-dom";
 import {clearCart} from "../../redux/actions/cartActions";
+import {IoCloseOutline} from "react-icons/io5";
+import Button from "../Home/Shared/Button";
 
 const ShoppingCart = (handleOrderPopup) => {
+  const [isLoading, setIsLoading] = useState(false);
   const current_user = useSelector(state => state.user.currentUser);
   const shoppingCart = useSelector(state => state.cart.shoppingCart);
-  const shippingCost = shoppingCart.length > 0 ? 4.99 : 0.00;
+  const shippingCost = shoppingCart.length > 0 ? 0.00 : 0.00;
   const dispatch = useDispatch();
-  console.log(current_user)
-  console.log(shoppingCart)
 
   React.useEffect(() => {
     AOS.init({
@@ -26,6 +27,7 @@ const ShoppingCart = (handleOrderPopup) => {
 
   const handleCheck = async () => {
     // const checkoutUrl = 'https://buy.stripe.com/test_4gw6p8cEA8555nW7st';
+    setIsLoading(true);
     try {
         const response = await axios.post('/api/checkout', {
           username: current_user.username,
@@ -47,6 +49,7 @@ const ShoppingCart = (handleOrderPopup) => {
             console.error('Error:', error.message);
         }
     }
+    setIsLoading(false);
   }
   const calculateSubtotal = () => {
     return shoppingCart.reduce((total, item) => {
@@ -84,88 +87,117 @@ const ShoppingCart = (handleOrderPopup) => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 pt-20">
-      <h1 className="mb-10 text-center text-2xl font-bold dark:text-slate-500">Cart Items</h1>
-      <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
-        <div className="rounded-lg md:w-2/3">
-          {(shoppingCart.length === 0) && (
-            <div data-aos="flip-left" data-aos-delay={100} className="dark:shadow-cyan-500/50 shadow-xl justify-center mb-6 rounded-lg dark:bg-gray-400 bg-white p-6 shadow-md sm:flex sm:justify-center">
-                <div className="flex items-center text-center border rounded-lg h-96 dark:border-gray-700">
-                  <div className="flex flex-col w-full px-16 mx-auto">
-                    <div className="p-3 mx-auto dark:text-slate-600 rounded-full">
-                      <AiOutlineFileExcel className="w-20 h-20"/>
-                    </div>
-                    <h1 className="mt-3 text-lg text-gray-800 dark:text-slate-600">No items found</h1>
-                    <p className="mt-2 text-gray-500 dark:text-gray-700">You can browse “Shop” to add some items to your shopping cart.</p>
-                    <div className="flex justify-center items-center mt-4 sm:mx-auto gap-x-3">
-                      <Link to="/">
-                        <button
-                          className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                               stroke="currentColor" className="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
-                          </svg>
-                          <span>Shopping now</span>
-                        </button>
-                      </Link>
+    <div className="shopping-cart-container">
+      {/* 加载覆盖层 */}
+      {isLoading && (
+        <div className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm">
+          <div id="loading" aria-label="Loading..." role="status" className="pt-32 flex flex-col items-center space-x-2">
+            <svg className="h-40 w-40 animate-spin stroke-gray-500" viewBox="0 0 256 256">
+              <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="24"></line>
+              <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="24"></line>
+              <line x1="224" y1="128" x2="192" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
+              </line>
+              <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="24"></line>
+              <line x1="128" y1="224" x2="128" y2="192" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
+              </line>
+              <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="24"></line>
+              <line x1="32" y1="128" x2="64" y2="128" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="24"></line>
+              <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="24">
+              </line>
+            </svg>
+            <span className="text-4xl font-medium text-gray-500">Loading...</span>
+          </div>
+        </div>
+      )}
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 pt-20">
+        <h1 className="mb-10 text-center text-2xl font-bold dark:text-slate-500">Cart Items</h1>
+        <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+          <div className="rounded-lg md:w-2/3">
+            {(shoppingCart.length === 0) && (
+              <div data-aos="flip-left" data-aos-delay={100} className="dark:shadow-cyan-500/50 shadow-xl justify-center mb-6 rounded-lg dark:bg-gray-400 bg-white p-6 shadow-md sm:flex sm:justify-center">
+                  <div className="flex items-center text-center border rounded-lg h-96 dark:border-gray-700">
+                    <div className="flex flex-col w-full px-16 mx-auto">
+                      <div className="p-3 mx-auto dark:text-slate-600 rounded-full">
+                        <AiOutlineFileExcel className="w-20 h-20"/>
+                      </div>
+                      <h1 className="mt-3 text-lg text-gray-800 dark:text-slate-600">No items found</h1>
+                      <p className="mt-2 text-gray-500 dark:text-gray-700">You can browse “Shop” to add some items to your shopping cart.</p>
+                      <div className="flex justify-center items-center mt-4 sm:mx-auto gap-x-3">
+                        <Link to="/">
+                          <button
+                            className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" className="w-5 h-5">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
+                            </svg>
+                            <span>Shopping now</span>
+                          </button>
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-          )}
-          {(shoppingCart.length > 0) && shoppingCart.map((item, index) => (
-            <div id="cart-detail" data-aos="flip-left" data-aos-delay={100} className="dark:shadow-cyan-500/50 shadow-xl justify-between mb-6 rounded-lg dark:bg-gray-400 bg-white p-6 shadow-md sm:flex sm:justify-start">
-            <img
-              src={item.img}
-              alt="product-image" className="w-full rounded-lg sm:w-40"/>
-            <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-              <div className="mt-5 sm:mt-0">
-                <h2 className="text-lg font-bold text-gray-900">{ item.title }</h2>
-                <p className="mt-1 text-xs text-gray-700">Stock: 23</p>
-              </div>
-              <div className="mt-4 flex justify-between sm:space-y-16 sm:mt-0 sm:block sm:space-x-0">
-                <div className="flex items-center border-gray-100">
-                  <span onClick={() => handleQuantityChange(item, -1)}
-                    className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
-                  <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={item.quantity}
-                         min="1"/>
-                  <span onClick={() => handleQuantityChange(item, 1)}
-                    className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
+            )}
+            {(shoppingCart.length > 0) && shoppingCart.map((item, index) => (
+              <div id="cart-detail" data-aos="flip-left" data-aos-delay={100} className="dark:shadow-cyan-500/50 shadow-xl justify-between mb-6 rounded-lg dark:bg-gray-400 bg-white p-6 shadow-md sm:flex sm:justify-start">
+              <img
+                src={item.img}
+                alt="product-image" className="w-full rounded-lg sm:w-40"/>
+              <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                <div className="mt-5 sm:mt-0">
+                  <h2 className="text-lg font-bold text-gray-900">{ item.title }</h2>
+                  <p className="mt-1 text-xs text-gray-700">Stock: 23</p>
                 </div>
-                <div className="flex items-center space-x-4">
-                  <p className="text-sm">unit price: ${item.price}</p>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" onClick={() => removeFromCart(item)}
-                       stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
-                  </svg>
+                <div className="mt-4 flex justify-between sm:space-y-16 sm:mt-0 sm:block sm:space-x-0">
+                  <div className="flex items-center border-gray-100">
+                    <span onClick={() => handleQuantityChange(item, -1)}
+                      className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
+                    <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={item.quantity}
+                           min="1"/>
+                    <span onClick={() => handleQuantityChange(item, 1)}
+                      className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
+                  </div>
+                  <div className="flex items-center space-x-4">
+                    <p className="text-sm">unit price: ${item.price}</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" onClick={() => removeFromCart(item)}
+                         stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                    </svg>
+                  </div>
                 </div>
               </div>
             </div>
+            ))}
           </div>
-          ))}
-        </div>
-        <div data-aos="flip-left" data-aos-delay={100} className="dark:shadow-cyan-500/50 shadow-xl mt-6 h-full rounded-lg border dark:bg-gray-400 bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-          <div className="mb-2 flex justify-between">
-            <p className="text-gray-700">Subtotal</p>
-            <p className="text-gray-700">${calculateSubtotal()}</p>
-          </div>
-          <div className="flex justify-between">
-            <p className="text-gray-700">Shipping</p>
-            <p className="text-gray-700">${shippingCost}</p>
-          </div>
-          <hr className="my-4"/>
-          <div className="flex justify-between">
-            <p className="text-lg font-bold">Total</p>
-            <div className="">
-              <p className="mb-1 text-lg font-bold">${calculateTotal()} USD</p>
-              <p className="text-sm text-gray-700">including tax</p>
+          <div data-aos="flip-left" data-aos-delay={100} className="dark:shadow-cyan-500/50 shadow-xl mt-6 h-full rounded-lg border dark:bg-gray-400 bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+            <div className="mb-2 flex justify-between">
+              <p className="text-gray-700">Subtotal</p>
+              <p className="text-gray-700">${calculateSubtotal()}</p>
             </div>
+            <div className="flex justify-between">
+              <p className="text-gray-700">Shipping</p>
+              <p className="text-gray-700">${shippingCost}</p>
+            </div>
+            <hr className="my-4"/>
+            <div className="flex justify-between">
+              <p className="text-lg font-bold">Total</p>
+              <div className="">
+                <p className="mb-1 text-lg font-bold">${calculateTotal()} USD</p>
+                <p className="text-sm text-gray-700">including tax</p>
+              </div>
+            </div>
+            <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600 disabled:cursor-not-allowed"
+                    onClick={handleCheck} disabled={shoppingCart.length === 0}>
+              Check out
+            </button>
           </div>
-          <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600 disabled:cursor-not-allowed"
-                  onClick={handleCheck} disabled={shoppingCart.length === 0}>
-            Check out
-          </button>
         </div>
       </div>
     </div>
